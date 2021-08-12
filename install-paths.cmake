073678b14c7abe2e0b00ec6fd6103d8b19fce511
friendlyanon <friendlyanon@users.noreply.github.com>
2021-08-12 18:08:29 +0200

    Use DATADIR instead of DATAROOTDIR

--- install-paths.cmake
+++ install-paths.cmake
@@ -1,4 +1,4 @@
 get_filename_component(absolute_prefix "${CMAKE_INSTALL_PREFIX}" ABSOLUTE)
 file(WRITE "${absolute_prefix}/${cmakedir}/${package}Paths.cmake" "\
-set(ASSET_PATH [[${absolute_prefix}/${datarootdir}/${package}]])
+set(ASSET_PATH [[${absolute_prefix}/${datadir}/${package}]])
 ")
